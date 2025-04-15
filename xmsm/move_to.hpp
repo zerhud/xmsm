b05117de297df7ee2723f9556982cf4524a84c2d
zerhud <zerhud@gmail.com>
2025-04-15 12:42:20 +0300

    implement extra checks in move_to with multi scenario

--- xmsm/move_to.hpp
+++ xmsm/move_to.hpp
@@ -94,60 +94,90 @@ struct state_queue_tracker {
 
 template<typename factory, typename list, typename scenario, typename target_state, typename fail_state>
 struct state_queue_tracker_multi {
-  int sum{0}, max_steps{-1}, n{0};
+  struct report_info {
+    int sum{0}, max_steps{-1}, n{0}, ind_multi_sf{0}, ind_multi_sb{0}, left_sum{0}, right_sum{0};
+    constexpr void upd(const report_info& other) {
+      auto upd = [](auto& orig, auto& cur) { orig = orig*(orig<=cur) + cur*(cur<orig); };
+      upd(sum, other.sum);
+      upd(max_steps, other.max_steps);
+      upd(n, other.n);
+      upd(ind_multi_sf, other.ind_multi_sf);
+      upd(ind_multi_sb, other.ind_multi_sb);
+      upd(left_sum, other.left_sum);
+      upd(right_sum, other.right_sum);
+    }
+    constexpr bool operator<=(const report_info& other) const {
+      return sum <= other.sum
+      && max_steps <= other.max_steps
+      && n <= other.n
+      && ind_multi_sf <= other.ind_multi_sf
+      && ind_multi_sb <= other.ind_multi_sb
+      && left_sum <= other.left_sum
+      && right_sum <= other.right_sum
+      ;
+    }
+    constexpr static auto count_to_end(auto h, auto _st_list) {
+      auto st_list = revert(_st_list);
+      return [&]<auto... inds>(std::index_sequence<inds...>){
+        return (0+...+( inds*(hash<factory>(get<inds>(st_list))==h) ));
+      }(std::make_index_sequence<size(st_list)>{});
+    }
+    constexpr static auto scenario_maximum(const auto& s) {
+      auto cur_hash = s.cur_state_hash();
+      auto cur_max = 0;
+      [&]<auto... inds>(std::index_sequence<inds...>){
+        ([&](auto ind, auto st_list) {
+          auto cur = count_to_end(cur_hash, st_list());
+          cur_max = cur*(cur_max<cur);
+        }(inds, get<inds>(list{})), ...);
+      }(std::make_index_sequence<size(list{})>{});
+      return cur_max;
+    }
+    constexpr void calculate(const auto& s) {
+      *this = report_info{};
+      n = s.count();
+      if (n==0) return ;
+      auto ind=0;
+      auto half = n/2;
+      s.foreach_scenario([&](const auto& s) {
+        auto cur_max = scenario_maximum(s);
+        max_steps = cur_max*(max_steps<cur_max);
+        sum += cur_max;
+        sum *= cur_max>0;
+        ind_multi_sf += cur_max*(ind+1);
+        ind_multi_sb += cur_max*(n-ind);
+        right_sum = right_sum*(ind<=half) + (right_sum+cur_max)*(half<ind);
+        left_sum = left_sum*(half<=ind) + (left_sum+cur_max)*(ind<half);
+      });
+    }
+  };
+
+  report_info info;
 
   consteval static bool is_valid() {return size(list{})>0;}
 
   template<typename tgt_sc, typename tgt_st, typename fail_st> constexpr state_queue_tracker_multi* search()
   requires(type_c<tgt_sc> == type_c<scenario> && type_c<tgt_st> == type_c<target_state> && type_c<fail_st> == type_c<fail_state>){ return this; }
-  constexpr bool deactivate() {return sum=0, max_steps=-1, n=0; }
-  constexpr bool is_active() const {return max_steps>0;}
-  constexpr auto count_to_end(auto h, auto _st_list) {
-    auto st_list = revert(_st_list);
-    return [&]<auto... inds>(std::index_sequence<inds...>){
-      return (0+...+( inds*(hash<factory>(get<inds>(st_list))==h) ));
-    }(std::make_index_sequence<size(st_list)>{});
-  }
-  constexpr auto scenario_maximum(const auto& s) {
-    auto cur_hash = s.cur_state_hash();
-    auto cur_max = 0;
-    [&]<auto... inds>(std::index_sequence<inds...>){
-      ([&](auto ind, auto st_list) {
-        auto cur = count_to_end(cur_hash, st_list());
-        cur_max = cur*(cur_max<cur);
-      }(inds, get<inds>(list{})), ...);
-    }(std::make_index_sequence<size(list{})>{});
-    return cur_max;
-  }
+  constexpr void deactivate() { info=report_info{}; }
+  constexpr bool is_active() const {return info.max_steps>0;}
   constexpr void activate(const auto& ms) {
     if (is_active()) return;
-    n = ms.count();
-    if (n==0) return ;
-    ms.foreach_scenario([this](const auto&s) {
-      auto cur_max = scenario_maximum(s);
-      max_steps = cur_max*(max_steps<cur_max);
-      sum += cur_max;
-    });
+    info.calculate(ms);
   }
   template<typename other_scenario, typename user_type> constexpr bool update(const xmsm::scenario<factory, other_scenario, user_type>& s) {
     return false;
   }
   template<typename user_type> constexpr bool update(const xmsm::scenario<factory, scenario, user_type>& s) {
-    int max = 0, sum = 0;
-    s.foreach_scenario([&](const auto&s) {
-      auto cur_max = scenario_maximum(s);
-      max = max*(max>cur_max) + cur_max*(max<=cur_max);
-      sum += cur_max;
-      sum *= cur_max>0;
-    });
-    this->sum = this->sum*(this->sum<=sum) + sum*(sum<this->sum);
-    max_steps = max_steps*(max_steps<=max) + max*(max<max_steps);
-    return !(this->sum>0 && sum <= this->sum && max <= max_steps);
+    report_info cur_info{};
+    cur_info.calculate(s);
+    auto ret = cur_info.sum > 0 && cur_info <= this->info;
+    info.upd(cur_info);
+    return !ret;
   }
   constexpr void update(auto* cur_scenario, const auto& e, auto&&... others) {
     if (!is_active()) return;
     if constexpr (!basic_scenario<factory,scenario>::is_finish_state(type_c<target_state>)) {
-      if (utils::count_from_set<factory,scenario>(others...) < n) {
+      if (utils::count_from_set<factory,scenario>(others...) < info.n) {
         fail(cur_scenario, e, others...);
         return;
       }
